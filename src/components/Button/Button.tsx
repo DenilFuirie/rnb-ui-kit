b34c2bf68e431c03685688256e81527fb492af64
DenilFuirie <rusm9snik2013@yandex.ru>
2022-02-07 13:17:59 +0300

    init

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+
+export interface ButtonProps {
+    color: string;
+};
+
+const Button: FC<ButtonProps> = ({ children, ...props }) => {
+    const { color } = props;
+    return (
+        <button {...props} style={{color}}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
