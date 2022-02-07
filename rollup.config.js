3b52178fcf3f6bc7cfa0f473dd547ff166d52462
DenilFuirie <rusm9snik2013@yandex.ru>
2022-02-07 19:17:23 +0300

    feat: add button

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+// NEW
+import scss from 'rollup-plugin-scss'
+
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 const packageJson = require("./package.json");
 
 export default [
@@ -21,14 +27,24 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
+
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+
+            // NEW
+            scss(), // will output compiled styles to output.css
+
+            terser(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+
+        // NEW
+        external: [/\.css|scss$/],
     },
 ];
