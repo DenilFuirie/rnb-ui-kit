c5213a1728284bce0a15978f36ba2706da55e2ed
DenilFuirie <rusm9snik2013@yandex.ru>
2022-02-08 11:57:12 +0300

    feat: add more props to button

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,37 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
-// NEW
-import scss from 'rollup-plugin-scss'
-
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-
-            // NEW
-            scss(), // will output compiled styles to output.css
-
-            terser(),
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-
-        // NEW
-        external: [/\.css|scss$/],
-    },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,76 @@
 import React, { FC } from 'react';
+import './Button.scss';
+import cn from 'clsx';
 
 export interface ButtonProps {
-    color: string;
-};
+    color?: 'primary' | 'secondary';
+    className?: string;
+    size?: 's' | 'lg';
+    disabled?: boolean;
+    children: string;
+    type?: 'button' | 'submit';
+    fullWidth?: boolean;
+    to?: string;
+    href?: string;
+    onClick?: () => void;
+    iconLeft?: string;
+    iconRight?: string;
+}
 
 const Button: FC<ButtonProps> = ({ children, ...props }) => {
-    const { color } = props;
+  const {
+    color = 'primary', className, disabled, type = 'button', size, fullWidth, onClick, href, iconLeft, iconRight,
+  } = props;
+
+  const handleClick = () => {
+    if (disabled) return;
+    if (!onClick) return;
+    onClick();
+  };
+  if (href) {
     return (
-        <button {...props} style={{color}}>
-            {children}
-        </button>
+      <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className={
+            cn(
+              'button',
+              className,
+              color,
+              size,
+              fullWidth ? 'fullwidth' : '',
+              disabled ? 'disabled' : '',
+            )
+        }
+      >
+        {iconLeft && <img src={iconLeft} className="button_iconLeft" alt="" />}
+        {children}
+        {iconRight && <img src={iconRight} className="button_iconRight" alt="" />}
+      </a>
     );
+  }
+
+  return (
+    <button
+      className={
+            cn(
+              'button',
+              className,
+              color,
+              size,
+              fullWidth ? 'fullwidth' : '',
+            )
+        }
+      type={type}
+      disabled={disabled}
+      onClick={handleClick}
+    >
+      {iconLeft && <img src={iconLeft} className="button_iconLeft" alt="" />}
+      {children}
+      {iconRight && <img src={iconRight} className="button_iconRight" alt="" />}
+    </button>
+  );
 };
 
 export default Button;
